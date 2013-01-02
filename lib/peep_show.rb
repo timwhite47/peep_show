15e356b0818ab660d0399c9ccb8b49985c9f1a53
Tim White <timwhite47@gmail.com>
2013-01-01 20:46:18 -0800

    git init

---
+++ lib/peep_show.rb
@@ -0,0 +1,21 @@
+module PeepShow
+
+  module ClassMethods
+    
+    def set_peep_show(*args)
+      options = {}
+      options.merge!(args.pop) if args.last.kind_of? Hash
+
+      define_method :og_obj do
+        {
+          :foo => "bar"
+        }
+      end
+    end
+    
+  end
+end
+
+class ActiveRecord::Base
+  include PeepShow
+end
