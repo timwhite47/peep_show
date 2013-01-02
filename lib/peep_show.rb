b0f4df281a33638ca0c994ab5e385e03c5c144ff
Tim White <timwhite47@gmail.com>
2013-01-01 22:53:22 -0800

    mapping basic info

--- lib/peep_show.rb
+++ lib/peep_show.rb
@@ -7,8 +7,40 @@ module PeepShow
       options = { }
       preview = args.pop if args.last.kind_of? Hash
       
+      fb_basic = {
+        :title => preview[:title], 
+        :type => "website", 
+        :image => preview[:image], 
+        :url => preview[:url],
+        :description => preview[:description], 
+      }
+
+      twitter_basic = {
+        :site => preview[:twitter].try(:[], :site_handle),
+        :author => preview[:twitter].try(:[], :author_handle),
+        :card => 'summary',
+        :title => preview[:title], 
+        :description => preview[:description],
+        :url => preview[:url],
+        :image => preview[:image]
+      }
+      # case args.first
+      # when :basic
+      #   fb = fb_basic
+      #   twitter = twitter_basic
+      # when :article
+          
+      # when :image
+
+      # when :video        
+        
+      # end
+      
       define_method :preview do
-      	preview
+      	{
+          fb: fb_basic,
+          twitter: twitter_basic
+        }
       end
     end    
 end
