e8b557bc8daaec149a0389041a0f18bb91856248
Tim White <timwhite47@gmail.com>
2013-01-01 21:46:54 -0800

    extend

--- lib/peep_show.rb
+++ lib/peep_show.rb
@@ -1,4 +1,8 @@
-module PeepShow    
+module PeepShow
+    def self.included(base)
+      base.extend(self)
+    end
+    
     def set_peep_show(*args)
       options = { }
       options.merge!(args.pop) if args.last.kind_of? Hash
