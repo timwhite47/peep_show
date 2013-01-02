452d2bed2a2af2ef4a4840701edfb8a86b1c2ac0
Tim White <timwhite47@gmail.com>
2013-01-01 21:17:42 -0800

    init.rb

--- lib/peep_show.rb
+++ lib/peep_show.rb
@@ -2,7 +2,7 @@ module PeepShow
 
   module ClassMethods
     
-    def set_peep_show(*args)
+    def self.set_peep_show(*args)
       options = {}
       options.merge!(args.pop) if args.last.kind_of? Hash
 
