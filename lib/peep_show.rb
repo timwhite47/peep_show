bfa3e076b91d2d049d9e0e130e8a0596e7241de5
Tim White <timwhite47@gmail.com>
2013-01-01 21:50:11 -0800

    playing with args

--- lib/peep_show.rb
+++ lib/peep_show.rb
@@ -2,13 +2,13 @@ module PeepShow
     def self.included(base)
       base.extend(self)
     end
-    
+
     def set_peep_show(*args)
       options = { }
       options.merge!(args.pop) if args.last.kind_of? Hash
       define_method :foobar do
       	{
-      		:foo => "bar"
+      		:foo => args.first
       	}
       end
     end    
